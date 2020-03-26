5d2a85259be7469877bcd3f41f5caae80ef75454
= <ashillpathak@gmail.com>
2020-03-26 16:46:44 +0000

    update

--- Conditionals.java
+++ Conditionals.java
@@ -4,21 +4,53 @@ public class Conditionals {
 	
 
 	
-	public int Decider(int num1, int num2, Boolean outcome) {
+	public void Decider(int num1) {
 		
 		
-		if (num1 % 2 == 0 || num2 % 2 == 0) {
+		if (num1 > 2000 && num1 <= 6000 && num1 <= 4000) {
 			
-			return 0;
+			System.out.println("A \nB \nE");
+		} else if (num1 > 6000) {
 			
-		} else if (outcome) {
+			System.out.println("A \nC");
+		} else if (num1>4000) {
+			System.out.println("A \nB \nD");
+		} else if (num1 <= 2000 && num1 > 100 && num1 >600) {
+			System.out.println("1 \n3 \n5");
+		} else if (num1 > 100 && num1 <= 500) {
+			System.out.println("1 \n3 \n4 \n7");
+		}else if (num1 > 500) {
+			System.out.println("1 \n3 \n4 \n6");
+		} else {
+			System.out.println("1 \n2");
+		}
 			
-			return num1 + num2;
+			
+}
+	
+	public void diffMeth(int n1) {
+		
+		if (n1 >= 0) {
+			
+			if (n1 > 2000 && n1 <= 4000)
+				System.out.println("A \nB \nE");
+			
+			if (n1 <= 6000 && n1 > 4000)
+				System.out.println("A \nB \nD");
+			
+			if (n1 > 6000)
+				System.out.println("A \nC");
+			
+			if (n1 > 100 && n1 <= 500)
+				System.out.println("1 \n3 \n4 \n7");
+			if (n1 > 500 && n1 <= 600)
+				System.out.println("1 \n3 \n4 \n6");
+			if (n1 > 600 && n1 <=2000)
+				System.out.println("1 \n3 \n5");
 			
 		} else {
-			
-			return num1 * num2;
+			System.out.println("1 \n2");
 		}
 	}
-
+	
 }
--- runner.java
+++ runner.java
@@ -4,10 +4,10 @@ public class runner {
 
 	public static void main(String[] args) {
 		
-		Conditionals test1 = new Conditionals();
+		Conditionals test = new Conditionals();
 		
-		System.out.println("First Test: ");
-		System.out.println(test1.Decider(7, 9,true ));
+		test.diffMeth(2040);
+
 
 	}
 
